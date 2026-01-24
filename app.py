e60bce8625dcbd327beaaa9f54601cd588973154
Akki Garg <akshit.garg@rutgers.edu>
2026-01-24 18:32:54 -0500

    Added search autocomplete functionality using omdb api. Currently still only puts simple title into list

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 # Imports
-from flask import Flask, render_template, request, redirect, url_for
+import os, requests
 import database
+
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from dotenv import load_dotenv
 
 # App
 app = Flask(__name__)
@@ -8,14 +11,76 @@ app = Flask(__name__)
 # Database
 database.init_db()
 
+# OMDb
+load_dotenv()
+OMDB_API_KEY = os.getenv("OMDB_API_KEY")
+base_url = f"http://www.omdbapi.com/?apikey={OMDB_API_KEY}&"
+
+@app.route('/search_movie')
+def search_movie():
+    query = request.args.get("q", "").strip()
+
+    if not query:
+        return jsonify(["not query"])
+
+    response = requests.get(f"{base_url}s={query}&type=movie", timeout=5)
+    data = response.json()
+
+    if not (response.status_code == 200):
+        return jsonify(["status code not 200"])
+
+    if data.get("Response") == "False":
+        return jsonify(data)
+
+    results = []
+    for item in data.get("Search", [])[:10]:
+        results.append({
+            "title": item.get("Title"),
+            "year": item.get("Year"),
+            "imdbID": item.get("imdbID"),
+            "poster": item.get("Poster") if item.get("Poster") != "N/A" else "/static/no-poster.png"
+        })
+
+    return jsonify(results)
+
+@app.route('/search_series')
+def search_series():
+    query = request.args.get("q", "").strip()
+
+    if not query:
+        return jsonify(["not query"])
+
+    response = requests.get(f"{base_url}s={query}&type=series", timeout=5)
+    data = response.json()
+
+    if not (response.status_code == 200):
+        return jsonify(["status code not 200"])
+
+    if data.get("Response") == "False":
+        return jsonify(data)
+
+    results = []
+    for item in data.get("Search", [])[:5]:
+        results.append({
+            "title": item.get("Title"),
+            "year": item.get("Year"),
+            "imdbID": item.get("imdbID"),
+            "poster": item.get("Poster") if item.get("Poster") != "N/A" else "/static/no-poster.png"
+        })
+
+    return jsonify(results)
 
 @app.route('/')
 def index():
     all_movies = database.get_all_movies()
     all_tv_shows = database.get_all_tv_shows()
     all_anime = database.get_all_anime()
-    return render_template("index.html", movie_list=all_movies, tv_show_list=all_tv_shows, anime_list=all_anime)
-
+    args = {
+        "movie_list": all_movies,
+        "tv_show_list": all_tv_shows,
+        "anime_list": all_anime
+    }
+    return render_template("index.html", **args)
 
 @app.route('/add_movie', methods=['POST'])
 def add_movie():
--- static/scripts.js
+++ static/scripts.js
@@ -38,14 +38,17 @@ function close_overlay_on_esc (event) {
 
 function edit_overlay_on() {
     document.getElementById("edit_overlay").style.display = "block";
+    document.getElementById("new_name").value = "";
 }
 
 function edit_overlay_off() {
     document.getElementById("edit_overlay").style.display = "none";
+    autocomplete_list.innerHTML = "";
 }
 
 function add_overlay_on() {
     document.getElementById("add_overlay").style.display = "block";
+    document.getElementById("add_name").value = "";
 }
 
 function add_overlay_off() {
@@ -60,6 +63,96 @@ function edit_pass_id(event) {
 
 // Passes data type (movie/tv_show/anime) to add form
 function add_type(event) {
-    type = event.target.dataset.type;
+    let type = event.target.dataset.type;
     document.getElementById("addForm").action = `/add_${type}`;
+}
+
+/*
+ * AUTOCOMPLETE SEARCH
+ */
+
+const add_name = document.getElementById("add_name");
+const autocomplete_list = document.getElementById("autocomplete_list");
+
+let search_timer = null;
+
+// Listens to the add_form
+add_name.addEventListener("input", function () {
+    const query = add_name.value.trim();
+    console.log("Typing:", query);
+
+    if (search_timer) clearTimeout(search_timer);
+
+    if (query.length < 4) {
+        autocomplete_list.innerHTML = "";
+        return;
+    }
+
+    search_timer = setTimeout(() => {
+        fetchResults(query);
+    }, 300);
+});
+
+
+async function fetchResults(query) {
+    console.log("Fetching for:", query);
+    const formAction = document.getElementById("addForm").action;
+    let type = formAction.split("_").pop();
+
+    if ((type == "show") || (type == "anime")) type = "series";
+    type.trim();
+
+    if(type == "movie"){
+        try {
+        const response = await fetch(`/search_movie?q=${encodeURIComponent(query)}`);
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        renderResults(data);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    if(type == "series"){
+        try {
+        const response = await fetch(`/search_series?q=${encodeURIComponent(query)}`);
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        renderResults(data);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+
+}
+
+function renderResults(results) {
+    autocomplete_list.innerHTML = "";
+
+    results.forEach(item => {
+        const li = document.createElement("li");
+
+        // image
+        const img = document.createElement("img");
+        img.src = item.poster;
+        img.alt = "Poster not found";
+
+        // text
+        const text = document.createElement("span");
+        text.textContent = `${item.title} | ${item.year}`
+
+        li.appendChild(img);
+        li.appendChild(text);
+
+        li.addEventListener("click", () => {
+            add_name.value = item.title;
+            autocomplete_list.innerHTML = "";
+        });
+
+        autocomplete_list.appendChild(li);
+    })
+
 }
